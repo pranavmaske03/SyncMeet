fb7e4cdb1b7f58038174a5c5a0383e009f8ec382
pranav <pranavbmaske@gmail.com>
2025-05-19 10:02:49 +0530

    Add real-time socket events: join room, code sync, video toggle, and peer connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Server } = require("socket.io");
 const cors = require('cors');
 const http = require('http');
 const path = require('path');
@@ -24,4 +25,89 @@ const getAllConnectedClients = (roomId) => {
         };
       }
     );
-  };
+};
+
+io.on("connection", (socket) => {
+    console.log(`Socket Connected`,socket.id);
+    socket.on("room:join", (data) => {
+        const { email, room } = data;
+      
+        emailToSocketIdMap.set(email, socket.id);
+        socketidToEmailMap.set(socket.id, email);
+      
+        io.to(room).emit("user:joined", { email, id: socket.id });
+        socket.join(room);
+        console.log(`${socket.id} joined ${room}`);
+        io.to(socket.id).emit("room:join", data);
+        const clients = getAllConnectedClients(room);
+        io.to(room).emit("new", { clients });
+    });      
+
+    socket.on("code:change", ({ roomId, code }) => {
+        console.log("code",code);
+        socket.in(roomId).emit("code:change", { code });
+        console.log(roomId);
+    });
+
+    // for handling video off event
+    socket.on("user:video:toggle", ({ to, isVideoOff, email }) => {
+        console.log("user:video:toggle", to, isVideoOff, email);
+        io.to(to).emit("remote:video:toggle", { isVideoOff, email });
+    });
+
+    socket.on("sync:code", ({ socketId, code }) => {
+        io.to(socketId).emit("code:change", { code });
+    });
+
+    socket.on("user:call", ({ to,offer,email }) => {
+        io.to(to).emit("incomming:call", {
+            from: socket.id,
+            offer,
+            fromEmail: email,
+        });
+    });
+
+    // handling code output
+    socket.on("output", ({ roomId,output }) => {
+        console.log("output",output);
+        socket.in(roomId).emit("output",{ output });
+    });
+
+    socket.on("language:change", ({ roomId,language,snippet }) => {
+        console.log(snippet,roomId);
+        socket.in(roomId).emit("language:change", { language,snippet});
+    });
+
+    socket.on("call:accepted", ({ to,ans }) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans});
+    });
+    
+    socket.on("peer:nego:needed", ({ to,offer}) => {
+        console.log("peer:nego:needed",offer);
+        io.to(to).emit("peer:nego:needed", { from: socket.id, offer});
+    });
+
+    socket.on("peer:nego:done", ({ to, ans}) => {
+        console.log("peer:nego:done",ans);
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans});
+    });
+
+    socket.on("leave:room", ({ roomId,email }) => {
+        socket.leave(roomId);
+        console.log(`${email} left ${roomId}`);
+        socket.to(roomId).emit("user:left", {email});
+    });
+
+    socket.on("wait:for:call", ({ to,email }) => {
+        console.log("wait:for:call", to,email);
+        io.to(to).emit("wait:for:call", { from: socket.id, email });
+    });
+
+    socket.on("disconnecting", () => {
+        io.emit("user:left", {id: socket.id});
+        console.log(`Socket Disconnected: ${socket.id}`);
+    });
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
