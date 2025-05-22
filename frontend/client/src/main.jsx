e3ed82e703ba300fc451fed526874bdf062d47f3
pranav <pranavbmaske@gmail.com>
2025-05-22 11:11:34 +0530

    feat: set up basic client-side routing with React Router

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { SocketProvider } from './contexts/SocketProvider.js'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render (
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <SocketProvider>
+        <App/>
+      </SocketProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
