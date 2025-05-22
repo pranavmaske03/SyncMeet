4bb1772ba0076d00573d235f0f95169c3e87589d
pranav <pranavbmaske@gmail.com>
2025-05-22 21:02:54 +0530

    fix: correct file extensions from .js to .jsx to resolve import issues

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { SocketProvider } from './contexts/SocketProvider.js'
+import { SocketProvider } from './contexts/SocketProvider.jsx'
 import App from './App.jsx'
 import './index.css'
 
